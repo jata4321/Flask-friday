556b44e4caa44013c0860edb06125a4b4e51938a
Kubuś <jata4321@gmail.com>
2023-01-22 13:49:19 +0100

    Database creation and before migration

--- hello.py
+++ hello.py
@@ -1,7 +1,37 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, flash
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 # Create Flask instance
 app = Flask(__name__)
+# Add database
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
+
+# Secret key
+app.config['SECRET_KEY'] = 'you want to change it in the future'
+
+db = SQLAlchemy(app)
+
+
+# Create Model
+class Users(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(30), nullable=False)
+    email = db.Column(db.String(30), nullable=False, unique=True)
+    date_added = db.Column(db.DateTime, default=datetime.utcnow)
+
+    # Create A String
+    def __repr__(self):
+        return f'<Name {self.name}'
+
+
+# Create Form Class
+class NameForm(FlaskForm):
+    name = StringField('Your name', validators=[DataRequired()])
+    submit = SubmitField('Submit')
 
 
 # Create a route decorator
@@ -21,17 +51,28 @@ def user(name):
     return render_template('user.html', name=name)
 
 
-# Custom Error Pages
+# Create Name Page with Form
+@app.route('/name', methods=['GET', 'POST'])
+def name():
+    name = None
+    form = NameForm()
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data = ''
+        flash('Form Submitted Correctly!')
+    return render_template('name.html', name=name, form=form)
 
+
+# Custom Error Pages
 # Invalid URL 404 error
 @app.errorhandler(404)
-def page_not_found(e):
+def page_not_found():
     return render_template('404.html'), 404
 
 
 # Invalid URL 500 error
 @app.errorhandler(500)
-def page_not_found(e):
+def page_not_found():
     return render_template('500.html'), 500
 
 
